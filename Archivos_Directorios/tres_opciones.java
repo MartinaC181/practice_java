5d9dfbf9d212ee45968fafdd72bad67a553b0a74
MartinaC <martiinacanteros@gmail.com>
2024-05-17 20:43:35 -0300

    Crear y listar archivos.

---
+++ Archivos_Directorios/tres_opciones.java
@@ -0,0 +1,85 @@
+//Crear un programa que tenga 3 opciones que se le muestra al usuario: 
+//1) Crear un archivo 2) Listar archivos 0) Salir
+//El programa debe capturar los posibles errores de escritura, por ejemplo,
+//el colocar una letra en donde deben colocar números o que no se pueda crear el archivo solicitado.
+package practice.Archivos_Directorios;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class tres_opciones {
+    public static void main(String[] args) {
+        Scanner leer= new Scanner(System.in);
+
+
+        System.out.println("--- MENU ---");
+        System.out.println("");
+        System.out.println("1 - Crear archivo");
+        System.out.println("2 - Listar archivos");
+        System.out.println("3 - Salir");
+        System.out.println("");
+
+        System.out.print("Seleccione una opción: ");
+        int opcion= leer.nextInt();
+
+        switch (opcion){
+
+            case 1:
+                System.out.println("Ingrese la extensión del archivo a crear");
+                String ext=leer.next();
+                System.out.println("Ahora el nombre");
+                String nombre=leer.next();
+                System.out.println("Ahora el destino del archivo");
+                String url= leer.next();
+
+                //Ruta del archivo que queremos crear
+                String archivoPath = url+"/"+nombre+"."+ext;
+
+                // Crear un objeto File con la ruta especificada
+                File archivo = new File(archivoPath);
+
+                try {
+                    // Crear el archivo
+                    archivo.createNewFile();
+                        System.out.println("Archivo creado exitosamente.");
+                } catch (IOException e) {
+                    System.out.println("Ocurrió un error al crear el archivo.");
+                    e.printStackTrace();
+                }
+                break;
+            case 2:
+                System.out.println("¿Qué directorio desea listar?");
+                String dir=leer.next();
+
+
+                // Crear un objeto File con la ruta especificada
+                File directorio = new File(dir);
+
+                // Obtener la lista de archivos y directorios
+                String[] lista = directorio.list();
+
+                if (lista != null) {
+                    for (String nombres : lista) {
+                        System.out.println(nombres);
+                    }
+                } else {
+                    System.out.println("El directorio está vacío o no existe.");
+                }
+                break;
+            case 3:
+                System.out.println("Gracias vuelva prontos");
+                break;
+            default:
+                System.out.println("Opcion no válida");
+                break;
+
+        }
+    }
+}
+
+
+
+   
+
